754fcd1ef9ce94133661f2f599b77242be0e002a
jaggy-1010 <jaggy1010@protonmail.com>
2024-05-15 15:12:31 +0200

    Zadanie 4 drugi commit

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Alicja";
+char sex = 'k';
+int age = 29;
+
+if( sex == 'k' && age < 30)
+{
+	Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+	Console.WriteLine("Ewa, lat 30");
+}
+else if(sex == 'm' && age < 18)
+{
+	Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+
