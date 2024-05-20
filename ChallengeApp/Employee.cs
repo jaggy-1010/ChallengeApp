6e919269f281abdbe950fdb74294e81be28431a8
jaggy-1010 <jaggy1010@protonmail.com>
2024-05-20 14:31:03 +0200

    Dzień 6  - Zadanie Domowe

---
+++ ChallengeApp/Employee.cs
@@ -0,0 +1,34 @@
+namespace ChallengeApp;
+
+class Employee
+{
+
+	private List<int> score = new List<int>();
+	
+	public Employee(string name, string surname, int age){
+		this.Name = name;
+		this.Surname = surname;
+		this.Age = age;
+		this.Sum = 0;
+	}
+
+	public string Name { get; private set; }
+	public string Surname { get; private set; }
+	public int Age { get; private set; }
+
+	public int Sum { get; private set; }
+
+	public void AddScore(int number)
+	{
+		this.score.Add(number);
+	}
+
+	public int Result
+	{
+		get
+		{
+			return this.score.Sum();
+		}
+	}
+}
+
--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,11 +1,43 @@
 ﻿using ChallengeApp;
 
-User user1 = new User("Adam", "32423fddf");
-User user2 = new User("Monika","32423fddf");
-User user3 = new User("Zuzia", "32423fddf");
-User user4 = new User("Damian", "32423fddf");
+Employee emp1 = new Employee("Jan", "Malinowski", 28);
+Employee emp2 = new Employee("Agata", "Kowalska", 27);
+Employee emp3 = new Employee("Maurycy", "Odyniec", 55);
 
-user1.AddScore(5);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
+List<Employee> employees = new List<Employee>();
+
+employees.Add(emp1);
+employees.Add(emp2);
+employees.Add(emp3);
+
+emp1.AddScore(7);
+emp1.AddScore(7);
+emp1.AddScore(8);
+emp1.AddScore(6);
+emp1.AddScore(8);
+
+emp2.AddScore(5);
+emp2.AddScore(8);
+emp2.AddScore(8);
+emp2.AddScore(8);
+emp2.AddScore(9);
+
+emp3.AddScore(6);
+emp3.AddScore(7);
+emp3.AddScore(8);
+emp3.AddScore(6);
+emp3.AddScore(7);
+
+var result1 = emp1.Result;
+var result2 = emp2.Result;
+var result3 = emp3.Result;
+
+List<int> sums = new List<int>();
+
+sums.Add(result1);
+sums.Add(result2);
+sums.Add(result3);
+
+var sumsMax = sums.Max();
+int winnerNumber = sums.IndexOf(sumsMax);
+Console.WriteLine("Imię: " + employees[winnerNumber].Name + ", nazwisko: " + employees[winnerNumber].Surname + ", wiek: " + employees[winnerNumber].Age + ",\nzdobyte punkty: " + employees[winnerNumber].Result);
