8313dcc05c00fb5e20f34b015060db8a6b7ddccf
jaggy-1010 <jaggy1010@protonmail.com>
2024-05-28 14:32:31 +0200

    Dzień 9 Zadanie Domowe - after removing unused files and folders

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -9,6 +9,6 @@ employee.AddScore(9);
 
 var statistics = employee.GetStatistics();
 
-Console.WriteLine($"Average score: {statistics.Average:N2}");
-Console.WriteLine($"Min score: {statistics.Min}");
-Console.WriteLine($"Max score: {statistics.Max}");
+Console.WriteLine($"{employee.Name} {employee.Surname} average score: {statistics.Average:N2}");
+Console.WriteLine($"{employee.Name} {employee.Surname} min score: {statistics.Min}");
+Console.WriteLine($"{employee.Name} {employee.Surname} max score: {statistics.Max}");
