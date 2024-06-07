3c4ad1e7fc2a1537c95e02fa2b0d0887e20600cf
jaggy-1010 <jaggy1010@protonmail.com>
2024-06-07 14:24:34 +0200

    After kopie folder removed

--- ChallengeApp/kopie/Statistics.cs
+++
@@ -1,9 +0,0 @@
-namespace ChallengeApp;
-
-public class Statistics
-{
-    public float Average { get; set; }
-    public float Min { get; set; }
-    public float Max { get; set; }
-    public char AverageLetter {get; set; }
-}
